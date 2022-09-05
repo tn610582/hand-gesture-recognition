07974061c216e6e6cdd447c6603b156783fdc9f0
Tianye Wang <tn610582@dal.ca>
2022-09-05 14:59:00 -0300

    Hand gesture main code

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
 import cv2
 import numpy as np
 import mediapipe as mp
+import math
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
+
+L1_diff = 0
+L2_diff = 0
+
+on_square = False
 
 #with?
 hands =  mp_hands.Hands(
@@ -14,8 +20,23 @@ hands =  mp_hands.Hands(
 
 cap = cv2.VideoCapture(0)
 
-rect_xx = (200,200)
-rect_yy = (300,300)
+# get width and height
+
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+rect_xx = 200
+rect_yy = 200
+rect_width = 150
+
+# Read images
+
+img_1 = cv2.imread('src/IMG_5889.jpg')
+img_2 = cv2.imread('src/IMG_5890.jpg')
+
+img_1 = cv2.resize(img_1, (140,300))
+img_2 = cv2.resize(img_2, (140,300))
+tmp_img = img_1
 
 while True:
     ret,image = cap.read()
@@ -41,11 +62,45 @@ while True:
                 mp_drawing_styles.get_default_hand_landmarks_style(),
                 mp_drawing_styles.get_default_hand_connections_style())
         # save 21 landmarks
-        for landmarks in hand_landmarks.landmark.x:
-            print(landmarks)
-    
-    cv2.rectangle(image,pt1 = rect_xx, pt2 = rect_yy, color=(0,200,0), thickness = -1)
+        x_s = []
+        y_s = []
+        for landmarks in hand_landmarks.landmark:
+            x_s.append(landmarks.x)
+            y_s.append(landmarks.y)
 
+        # get index finger coordinates
+        finger_8_x = int(x_s[8]*width)
+        finger_8_y = int(y_s[8]*height)
+        # get thumb coordinates
+        finger_4_x = int(x_s[4]*width)
+        finger_4_y = int(y_s[4]*height)
+
+        length = math.hypot((finger_8_x-finger_4_x),(finger_8_y-finger_4_y))
+
+        # Circle follow finger 8
+        # cv2.circle(image,(finger_8_x,finger_8_y),20,(200,0,0),-1)
+        print(finger_8_x,finger_8_y)
+
+        # Calculate finger on rectangle
+        if length < 100:
+            if (finger_8_x > rect_xx) and (finger_8_x<(rect_xx+rect_width)):
+                if(finger_8_y > rect_yy) and (finger_8_y < (rect_yy+rect_width)):
+                    if on_square == False:
+                            L1_diff = abs(finger_8_x - rect_xx)
+                            L2_diff = abs(finger_8_y - rect_yy)
+                            on_square = True   
+                            tmp_img = img_2
+        else: 
+            on_square = False
+            tmp_img = img_1
+
+        if on_square:    
+            rect_xx = finger_8_x - L1_diff
+            rect_yy = finger_8_y - L2_diff
+        
+    # Draw rectangle
+    #cv2.rectangle(image,pt1 = (rect_xx,rect_yy), pt2 = (rect_xx+rect_width,rect_yy+rect_width), color=(0,200,0), thickness = -1)
+    image[rect_yy:rect_yy+300, rect_xx:rect_xx+140] = tmp_img   
     cv2.imshow("Drag", image)
 
     if cv2.waitKey(10) & 0xFF == ord('q'):
